a8e11552648ee22288372d30dd9362f73be71981
mosh98 <mosleh.edu@gmail.com>
2023-03-07 13:53:02 +0100

    Initial commit

---
+++ src/main/java/com/example/mefit/MefitApplication.java
@@ -0,0 +1,13 @@
+package com.example.mefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MefitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MefitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mefit/MefitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MefitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
