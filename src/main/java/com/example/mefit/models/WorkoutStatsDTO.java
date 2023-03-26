a53c654a761121fea42a1e872b01e3d819ddb4d2
moslehmahamud <mosleh.edu@gmail.com>
2023-03-26 13:12:53 +0200

    scafolding for workout stats

---
+++ src/main/java/com/example/mefit/models/WorkoutStatsDTO.java
@@ -0,0 +1,17 @@
+package com.example.mefit.models;
+
+import java.util.HashMap;
+
+public class WorkoutStatsDTO {
+
+    /**
+     * This class is used to return the stats of a workout
+     */
+
+    private Integer remainingWorkouts;
+
+    private Integer CompletedWorkouts;
+
+    private Integer totalWorkouts;
+    private HashMap<String,Integer> muscleGroupStats;
+}
